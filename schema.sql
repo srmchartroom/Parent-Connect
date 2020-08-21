506a72b7a5a99b2ca630dc85951e3a476c1d23fc
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-08-20 20:51:13 -0400

    fixed conflicts in schema and config

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 -- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS parent_posts;
+DROP DATABASE IF EXISTS passport_demo;
 -- Creates the "blogger" database --
-CREATE DATABASE parent_posts;
+CREATE DATABASE passport_demo;
