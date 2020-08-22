370df5d8acdcdc228ff5e590f5ab57a9d8817876
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2020-08-22 13:42:38 -0400

    Added postform js to frontend for submit post

---
+++ public/js/postform.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  console.log("succesfully loaded postform.js");
+  let titleInput = $("#title");
+  let bodyInput = $("#body");
+  let linkInput = $("#link");
+  let categoryInput = $("#category");
+  let districtInput = $("#district");
+  let schoolInput = $("#school");
+  let gradeInput = $("#grade");
+  //add event listener when form is submitted
+  $("#post_form").on("submit", handleFormSubmit);
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    let newPost = {
+      user_id: "1",
+      title: titleInput.val(),
+      body: bodyInput.val(),
+      link: linkInput.val(),
+      category: categoryInput.val(),
+      district: districtInput.val(),
+      school: schoolInput.val(),
+      grade: gradeInput.val()
+    };
+    submitPost(newPost);
+    console.log(newPost);
+  }
+  function submitPost(post) {
+    $.post("/api/posts", post, function() {
+      window.location.href = "/members";
+    });
+  }
+});
